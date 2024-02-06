db0b409b8712cc891b3330331f2542ae21981a2e
AnzaKibriya <33507051+AnzaKibriya@users.noreply.github.com>
2024-02-06 16:20:08 +0400

    Order Details Page

--- src/main/java/Pages/OrderDetails.java
+++ src/main/java/Pages/OrderDetails.java
@@ -15,43 +15,43 @@ import java.io.Reader;
 import java.util.List;
 import java.util.Objects;
 
+import Enum.PendingMedicineInformationEnum;
+
+import static Helper.BaseClass.waitForLoaderInvisibility;
+
 public class OrderDetails {
-    int i = 2 ;
+    int i = 2;
     WebDriver driver;
-    @FindBy(xpath = "//*[@placeholder='Search by Attribute']")
-    WebElement searchBar;
     @FindBy(xpath = "//*[@id='mat-tab-content-0-1']//tbody/tr/td[8]/div/img[2]")
     WebElement actionButton;
     String medicinePendingInfoInTable = "//app-pending-medication-info//mat-drawer-content//tbody//td['" + i + "']";
+
     public OrderDetails(WebDriver Driver) {
         driver = Driver;
     }
-     public void searchForOrder(String orderID){
-        searchBar.sendKeys("811801715");
-        searchBar.sendKeys(Keys.ENTER);
-     }
-     public void openOrderDetailPage(){
-        actionButton.click();
-     }
-     public void verifyOrderDetailTable(){
-        List<WebElement> ele = driver.findElements(By.xpath(medicinePendingInfoInTable));
-//         PendingMedicineInformationEnum[] pendingMedicineInformationEnums = PendingMedicineInformationEnum.values();
-//         for (int i=2; i <=pendingMedicineInformationEnums.length;i++){
-//            String ABC = ele.get(i).getText();
-//            String CP = pendingMedicineInformationEnums[i-2].value;
-//            Assert.assertEquals(ABC, CP);
-//
-//         }
 
-         try (Reader reader = new InputStreamReader(Objects.requireNonNull(this.getClass()
-                 .getResourceAsStream("/test.json")))) {
-             TestModel[] result = new Gson().fromJson(reader, TestModel[].class);
-             for (int i=2; i <=ele.size();i++){
-//                 String AB = result[i-2].getName();
-                 String ABC = ele.get(i).getText();
-//                 Assert.assertEquals(ABC, AB);
-             }
-         } catch (IOException ignored) {
-         }
-     }
+    public void openOrderDetailPage() {
+        actionButton.click();
+        waitForLoaderInvisibility();
+    }
+
+    public void verifyOrderDetailTable() {
+        List<WebElement> ele = driver.findElements(By.xpath(medicinePendingInfoInTable));
+        PendingMedicineInformationEnum[] pendingMedicineInformationEnums = PendingMedicineInformationEnum.values();
+        for (int i = 2; i <= pendingMedicineInformationEnums.length; i++) {
+            String ABC = ele.get(i).getText();
+            String CP = pendingMedicineInformationEnums[i - 2].value;
+            Assert.assertEquals(ABC, CP);
+        }
+//         try (Reader reader = new InputStreamReader(Objects.requireNonNull(this.getClass()
+//                 .getResourceAsStream("/test.json")))) {
+//             TestModel[] result = new Gson().fromJson(reader, TestModel[].class);
+//             for (int i=2; i <=ele.size();i++){
+////                 String AB = result[i-2].getName();
+//                 String ABC = ele.get(i).getText();
+////                 Assert.assertEquals(ABC, AB);
+//             }
+//         } catch (IOException ignored) {
+//         }
+    }
 }
--- src/test/java/TestDawakCP.java
+++ src/test/java/TestDawakCP.java
@@ -5,17 +5,22 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 public class TestDawakCP extends BaseClass {
-    @Test
+    @Test(priority = 1)
     public void verifyCPLogin()throws IOException {
         test = extent.createTest("Login to Central Pharmacist");
         Pages.LoginCP().invalidCPLogin();
         Pages.LoginCP().CPLogin();
     }
-     @Test
-    public void verifyMakingOrderInProgress() throws IOException {
+     @Test (priority = 2)
+    public void verifyMakingOrderInProgress(){
         test = extent.createTest("Verify Making Order In Progress State");
         Pages.Home().verifyHomePageHeader();
         Pages.Home().SearchForOrder();
         Pages.Home().moveOrderToInProgressStateAndVerify();
+     }
+     @Test (priority = 3)
+    public void verifyOrderDetails(){
+        Pages.OrderDetails().openOrderDetailPage();
+        Pages.OrderDetails().verifyOrderDetailTable();
      }
 }
